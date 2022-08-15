8d822166b73e4266649b7220cb81c915037ce547
Minegabu <gabriel.montelle9@gmail.com>
2022-08-15 13:56:03 +1200

    Added display of update instead of just vote for

--- src/UserController.js
+++ src/UserController.js
@@ -4,10 +4,12 @@ const users = require('./models/users')
 const GetCandidate = async (request, response) => {
     try {
         const { id } = request.params
-        const foundUsers = await users.find({ email: id }, { division_id: 1, _id: 0 })
+        const foundUsers = await users.find({ email: id }, { division_id: 1, candidate_vote: 1, _id: 0 })
         let division_id = foundUsers[0].division_id
+        let existingVote = foundUsers[0].candidate_vote
         const target = await candidates.find({ division_id: division_id })
-        return response.status(200).json({ success: true, data: target })
+        let data = [existingVote, target]
+        return response.status(200).json({ success: true, data: data })
     } catch (error) {
         console.log(error)
         return response.status(400).send('ERROR')
