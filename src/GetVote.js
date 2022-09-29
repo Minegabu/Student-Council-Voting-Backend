0dda57ef0c334df76989cc215e6b2646242433cd
Minegabu <gabriel.montelle9@gmail.com>
2022-09-30 12:09:33 +1300

    changes to commit v like 812831

--- src/GetVote.js
+++ src/GetVote.js
@@ -4,12 +4,14 @@ const users = require('./models/users')
 const GetVote = async (request, response) => {
     try {
         // find all the votes
-        const vote = await users.find({}, { candidate_vote: 1, _id: 0 })
+        const vote = await users.find({}, { candidate_vote: 1, _id: 0 }, { sortBy: { candidate_vote: 1 } })
         const vote2 = vote.map((vote) => vote.candidate_vote)
         console.log(vote2)
         var counts = {};
+        counts.sort(function (first, second) {
+            return second[1] - first[1];
+        });
         vote2.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
-        console.log(counts)
         return response.status(200).json({ success: true, data: counts })
     } catch (err) {
         // if it fails to find all the votes
