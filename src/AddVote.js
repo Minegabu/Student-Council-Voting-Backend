31040126c7d590664ddf99733b76c13d49d4b87f
Minegabu <gabriel.montelle9@gmail.com>
2022-10-10 18:00:07 +1300

    Yes

--- src/AddVote.js
+++ src/AddVote.js
@@ -3,8 +3,9 @@ const users = require('./models/users')
 const AddVote = async (req, res) => {
     try {
         const { vote, name } = req.body;
-        const add = await users.updateOne({ email: email }, { $set: { "candidate_vote": vote } })
+        const add = await users.updateOne({ email: name }, { $set: { "candidate_vote": vote } })
         res.json({ add })
+        return res.status(200)
     } catch (error) {
         console.log(error)
         return res.status(400).send('Username already exists')
