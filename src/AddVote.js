1a6881956f4ca7a3e7c845eaa74720f82f4e1cba
Minegabu <gabriel.montelle9@gmail.com>
2022-10-10 17:42:38 +1300

    More fixes

--- src/AddVote.js
+++ src/AddVote.js
@@ -1,20 +1,15 @@
-// import things
 const users = require('./models/users')
 
 const AddVote = async (req, res) => {
     try {
-        // get vote from the post request
         const { vote, name } = req.body;
-        // add the vote to the database
-        const add = await users.updateOne({ name: name })
-
+        const add = await users.updateOne({ name: name }, { $set: { "candidate_vote": vote } })
+        console.log(add)
         res.json({ add })
     } catch (error) {
-        // return a failed response if it could not do that
         console.log(error)
         return res.status(400).send('Username already exists')
     }
 }
-
 
 module.exports = { AddVote }
